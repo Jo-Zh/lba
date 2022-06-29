169569d1e5b47f40ffa40ca20c836de81dcc0a0c
Jo-Zh <joannaweinazh@gmail.com>
2022-06-29 22:31:59 +1000

    Procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: python manage.py migrate
 web: gunicorn lba.wsgi --log-file -
--- lba/settings.py
+++ lba/settings.py
@@ -11,11 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-# from decouple import config
-with open('/etc/adminMail_lba.txt') as f:
-    USER_01 = f.readline().strip()
-    PASSWORD_01 = f.readline().strip()
-    EMAIL_01 = f.readline().strip()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,8 +22,6 @@ MEDIA_URL=('uploads/')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# with open('/etc/secret_key_lba.txt') as f:
-#     SECRET_KEY = f.read().strip()
 
 SECRET_KEY=os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -208,15 +201,15 @@ LOGGING = {
 }
 
 #deploy security setting
-# CORS_REPLACE_HTTPS_REFERER      = True
-# HOST_SCHEME                     = "https://"
-# SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT             = True
-# SESSION_COOKIE_SECURE           = True
-# CSRF_COOKIE_SECURE              = True
-# SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
-# SECURE_HSTS_SECONDS             = 1000000
-# SECURE_FRAME_DENY               = True
+CORS_REPLACE_HTTPS_REFERER      = True
+HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
