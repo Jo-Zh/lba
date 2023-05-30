1f50b72ce372639efbf4aeaa8ed7ef7f7280e8d8
Jo-Zh <joannaweinazh@gmail.com>
2023-05-30 18:51:06 +1000

    update

--- lba/settings.py
+++ lba/settings.py
@@ -21,9 +21,11 @@ MEDIA_URL=('uploads/')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-
-SECRET_KEY=os.environ.get("SECRET_KEY")
+try:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    
+except KeyError as e:
+    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
